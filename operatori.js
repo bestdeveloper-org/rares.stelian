4e61f4d28f3db3667a8ce893633a41ce736cb15f
Rares Stelian <raresstelian@gmail.com>
2017-12-01 19:57:56 +0200

    here are some of the opreators exercices too :)

---
+++ operatori.js
@@ -0,0 +1,52 @@
+"use strict"
+	//OPERATORI
+	//declarearea unor variabile
+// var a=prompt("scritei valaorea variabilei a");
+// var b=prompt("scritei valaorea variabilei b "); 
+// 	//operatori relationali
+// console.log("varibila a este " + a);
+// console.log("variabila b este " + b);
+// console.log("a+b este " + a + b);
+// console.log("a-b este " + (a - b));
+// console.log("a inmultit cu b este " + a * b);
+// console.log("a impartit la b este " + a / b);
+// console.log("restul impartirii lui a la b este " + a % b);
+// console.log("adunarea cu o unitate a lui a este " + ++a);
+// console.log("adunarea cu o unitate a lui b este " + ++b);
+// console.log("scaderea lui a cu o unitate este " + --a +" Atentie variabila a a fost incrementata mai sus ");
+// console.log("scaderea lui b cu o unitate este " + --b +" Atentie variabila b a fost incrementata mai sus ");
+// 	//operatori de asignare
+// console.log("rezultatul operatiei 'a+=b sau a=a+b ' este " + (a+=b));
+// console.log("rezultatul operatiei 'a-=b sau a=a-b ' este " + (a-=b));
+// console.log("rezultatul operatiei 'a*=b sau a=a*b ' este " + (a*=b));
+// console.log("rezultatul operatiei 'a/=b sau a=a/b ' este " + (a/=b));
+// console.log("rezultatul operatiei 'a%=b sau a=a%b ' este " + (a%=b));
+// 	//operatori de comparatie
+a=prompt("scritei valaorea variabilei a pentru operatorii de comapratie");
+b=prompt("scritei valaorea variabilei b pentru operatorii de comparatie");
+// if (a==b) {
+// 	console.log("a este egal cu b");
+// }	else{
+// 		console.log("a nu este egal cu b");
+
+// 	}
+
+// if (a!=b) {
+// 	console.log("a nu este egal cu b");
+// }	else{
+// 		console.log("a este egal cu b");
+
+// 	}
+
+if (a>b) {
+	console.log("a este mai mare decat b");
+}else{
+	if (a==b) {
+		console.log("a este egal b");
+	}else{
+		console.log("a este mai mic decat b");
+		 }	
+
+	}
+
+
